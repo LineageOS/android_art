181a187128f9b59b02c10891f41471008cb13932
Alex Naidis <alex.naidis@linux.com>
2016-08-20 00:19:57 +0200

    runtime: arch: support kryo for 32 bit
    * we will be using kryo as target cpu for 32 bit as we have improved arch flags and specific routines in bionic for kryo
    * mark kryo as supporting all arm 32 bit features, since it does support them
    
    * made together with Jake Weinstein (xboxfanj)
    
    Change-Id: Ic2324651bd161a5e35f2a94b6cf575e76bae24a1
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- runtime/arch/arm/instruction_set_features_arm.cc
+++ runtime/arch/arm/instruction_set_features_arm.cc
@@ -43,14 +43,14 @@ const ArmInstructionSetFeatures* ArmInstructionSetFeatures::FromVariant(
   static const char* arm_variants_with_div[] = {
           "cortex-a7", "cortex-a12", "cortex-a15", "cortex-a17", "cortex-a53", "cortex-a57",
           "cortex-a53.a57", "cortex-m3", "cortex-m4", "cortex-r4", "cortex-r5",
-          "cyclone", "denver", "krait", "swift" };
+          "cyclone", "denver", "krait", "kryo", "swift" };
 
   bool has_div = FindVariantInArray(arm_variants_with_div, arraysize(arm_variants_with_div),
                                     variant);
 
   // Look for variants that have LPAE support.
   static const char* arm_variants_with_lpae[] = {
-      "cortex-a7", "cortex-a15", "krait", "denver", "cortex-a53", "cortex-a57", "cortex-a53.a57"
+      "cortex-a7", "cortex-a15", "krait", "kryo", "denver", "cortex-a53", "cortex-a57", "cortex-a53.a57"
   };
   bool has_lpae = FindVariantInArray(arm_variants_with_lpae, arraysize(arm_variants_with_lpae),
                                      variant);
