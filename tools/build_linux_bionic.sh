3cddf4538a8df429a6084f7176c07d55e1e0ec67
Alex Light <allight@google.com>
2019-05-22 11:22:31 -0700

    Make sure a out/build_number.txt file is present with linux-bionic
    On some branches if out/build_number.txt must be present for
    linux_bionic targets to be built. This makes the build_linux_bionic.sh
    script create this file.
    
    Test: DIST_DIR=$PWD/fast-out/dist ./art/tools/dist_linux_bionic.sh showcommands com.android.runtime.host com.android.support.apexer
    Bug: 132822173
    Change-Id: Idc2e89e3947468f26d61d2db224910f3c13410d7

--- tools/build_linux_bionic.sh
+++ tools/build_linux_bionic.sh
@@ -57,6 +57,7 @@ host_out=$(get_build_var HOST_OUT)
 # There is no good way to force soong to generate host-bionic builds currently
 # so this is a hacky workaround.
 tmp_soong_var=$(mktemp --tmpdir soong.variables.bak.XXXXXX)
+tmp_build_number=$(cat ${out_dir}/build_number.txt)
 
 cat $out_dir/soong/soong.variables > ${tmp_soong_var}
 
@@ -87,5 +88,8 @@ json.dump(x, open(sys.argv[2], mode='w'))
 END
 
 rm $tmp_soong_var
+
+# Write a new build-number
+echo ${tmp_build_number}_SOONG_ONLY_BUILD > ${out_dir}/build_number.txt
 
 build/soong/soong_ui.bash --make-mode --skip-make $@
