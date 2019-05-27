0b07c693eb43fb906f6b31e42eccd03dfc0dec89
randy.jeong <randy.jeong@samsung.com>
2019-05-27 10:29:09 +0900

    [RUNTIME] Ignore signal registration for debugging
    It's difficult to identify native crash/error of 3rd party app.
    Because they can control their app with own signal handling.
    Therefore I would like to support the way to ignore signal
    registration in 3rd party app with the specific property.
    
    To enable this, do just setprop "debug.ignoreappsignalhandler 1".
    
    Test: test app to hook signal, then setprop debug.ignoreappsignalhandler 1
    
    Change-Id: I56e2f1255a71abae339396379deb8cb5c31c25c5
    Signed-off-by: randy.jeong <randy.jeong@samsung.com>

--- runtime/runtime.h
+++ runtime/runtime.h
@@ -692,6 +692,8 @@ class Runtime {
     is_native_debuggable_ = value;
   }
 
+  void SetSignalHookDebuggable(bool value);
+
   bool AreNonStandardExitsEnabled() const {
     return non_standard_exits_enabled_;
   }
