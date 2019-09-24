dd1d9ba1851d96a8d4f55cd8e983fcee1519d953
Nicholas Lim <nicknitewolf@lineageos.org>
2019-09-24 19:59:46 +0800

    art: Conditionally remove version check for memfd_create()
     * This check is redundant as devices that lack
       the memfd_create syscall will fail to boot
       regardless of the check.
    
    This conditionally reverts commit 51f89d9.
    
    Change-Id: Ife2cebde713e14bd787f48628092650866e2ac09

--- libartbase/base/memfd.cc
+++ libartbase/base/memfd.cc
@@ -17,11 +17,15 @@
 #include "memfd.h"
 
 #include <errno.h>
+#if !defined(HAS_MEMFD_BACKPORT)
 #include <stdio.h>
+#endif
 #if !defined(_WIN32)
 #include <fcntl.h>
 #include <sys/syscall.h>
+#if !defined(HAS_MEMFD_BACKPORT)
 #include <sys/utsname.h>
+#endif
 #include <unistd.h>
 #endif
 #if defined(__BIONIC__)
@@ -49,6 +53,7 @@ namespace art {
 #if defined(__NR_memfd_create)
 
 int memfd_create(const char* name, unsigned int flags) {
+#if !defined(HAS_MEMFD_BACKPORT)
   // Check kernel version supports memfd_create(). Some older kernels segfault executing
   // memfd_create() rather than returning ENOSYS (b/116769556).
   static constexpr int kRequiredMajor = 3;
@@ -62,6 +67,7 @@ int memfd_create(const char* name, unsigned int flags) {
     errno = ENOSYS;
     return -1;
   }
+#endif
 
   return syscall(__NR_memfd_create, name, flags);
 }
