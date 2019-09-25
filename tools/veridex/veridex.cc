hopez13/art

Allow excluding API list values via command line arguments.
This replaces --only-report-sdk-uses as well. Note only ApiList::Value and not DomainApi are supported for now.

Test: manual
Change-Id: I7734eae1ebb0f922eb4652d29f1e1e4dce18cbec

tools/veridex/veridex.cc
@@ -71,17 +71,17 @@ static const char* kStubsOption = "--core-stubs=";
 static const char* kFlagsOption = "--api-flags=";
 static const char* kImprecise = "--imprecise";
 static const char* kTargetSdkVersion = "--target-sdk-version=";
-static const char* kOnlyReportSdkUses = "--only-report-sdk-uses";
 static const char* kAppClassFilter = "--app-class-filter=";
+static const char* kExcludeApiListsOption = "--exclude-api-lists=";
 
 struct VeridexOptions {
   const char* dex_file = nullptr;
   const char* core_stubs = nullptr;
   const char* flags_file = nullptr;
   bool precise = true;
-  int target_sdk_version = 28; /* P */
-  bool only_report_sdk_uses = false;
+  int target_sdk_version = 29; /* P */
   std::vector<std::string> app_class_name_filter;
+  std::vector<std::string> exclude_api_lists;
 };
 
 static const char* Substr(const char* str, int index) {
@@ -108,13 +108,14 @@ static void ParseArgs(VeridexOptions* options, int argc, char** argv) {
       options->precise = false;
     } else if (StartsWith(argv[i], kTargetSdkVersion)) {
       options->target_sdk_version = atoi(Substr(argv[i], strlen(kTargetSdkVersion)));
-    } else if (strcmp(argv[i], kOnlyReportSdkUses) == 0) {
-      options->only_report_sdk_uses = true;
     } else if (StartsWith(argv[i], kAppClassFilter)) {
       options->app_class_name_filter = android::base::Split(
           Substr(argv[i], strlen(kAppClassFilter)), ",");
+    } else if (StartsWith(argv[i], kExcludeApiListsOption)) {
+      options->exclude_api_lists = android::base::Split(
+          Substr(argv[i], strlen(kExcludeApiListsOption)), ",");
     } else {
-      LOG(WARNING) << "Unknown command line argument: " << argv[i];
+      LOG(ERROR) << "Unknown command line argument: " << argv[i];
     }
   }
 }
@@ -196,9 +197,10 @@ class Veridex {
     Resolve(app_dex_files, resolver_map, type_map, &app_resolvers);
 
     ClassFilter app_class_filter(options.app_class_name_filter);
+    ApiListFilter api_list_filter(options.exclude_api_lists);
 
     // Find and log uses of hidden APIs.
-    HiddenApi hidden_api(options.flags_file, options.only_report_sdk_uses);
+    HiddenApi hidden_api(options.flags_file, api_list_filter);
     HiddenApiStats stats;
 
     HiddenApiFinder api_finder(hidden_api);
@@ -247,7 +249,7 @@ class Veridex {
       precise_api_finder.Dump(std::cout, &stats);
     }
 
-    DumpSummaryStats(std::cout, stats, options);
+    DumpSummaryStats(std::cout, stats, api_list_filter);
 
     if (options.precise) {
       std::cout << "To run an analysis that can give more reflection accesses, " << std::endl
@@ -260,20 +262,15 @@ class Veridex {
  private:
   static void DumpSummaryStats(std::ostream& os,
                                const HiddenApiStats& stats,
-                               const VeridexOptions& options) {
+                               const ApiListFilter& api_list_filter) {
     static const char* kPrefix = "       ";
-    if (options.only_report_sdk_uses) {
-      os << stats.api_counts[hiddenapi::ApiList::Whitelist().GetIntValue()]
-         << " SDK API uses." << std::endl;
-    } else {
-      os << stats.count << " hidden API(s) used: "
-         << stats.linking_count << " linked against, "
-         << stats.reflection_count << " through reflection" << std::endl;
-      for (size_t i = 0; i < hiddenapi::ApiList::kValueCount; ++i) {
-        hiddenapi::ApiList api_list = hiddenapi::ApiList(i);
-        if (api_list != hiddenapi::ApiList::Whitelist()) {
-          os << kPrefix << stats.api_counts[i] << " in " << api_list << std::endl;
-        }
+    os << stats.count << " hidden API(s) used: "
+       << stats.linking_count << " linked against, "
+       << stats.reflection_count << " through reflection" << std::endl;
+    for (size_t i = 0; i < hiddenapi::ApiList::kValueCount; ++i) {
+      hiddenapi::ApiList api_list = hiddenapi::ApiList(i);
+      if (api_list_filter.Matches(api_list)) {
+        os << kPrefix << stats.api_counts[i] << " in " << api_list << std::endl;
       }
     }
   }

LineageOS/android_art

tools/veridex/veridex.cc
@@ -71,17 +71,17 @@ static const char* kStubsOption = "--core-stubs=";
 static const char* kFlagsOption = "--api-flags=";
 static const char* kImprecise = "--imprecise";
 static const char* kTargetSdkVersion = "--target-sdk-version=";
-static const char* kOnlyReportSdkUses = "--only-report-sdk-uses";
 static const char* kAppClassFilter = "--app-class-filter=";
+static const char* kExcludeApiListsOption = "--exclude-api-lists=";
 
 struct VeridexOptions {
   const char* dex_file = nullptr;
   const char* core_stubs = nullptr;
   const char* flags_file = nullptr;
   bool precise = true;
-  int target_sdk_version = 28; /* P */
-  bool only_report_sdk_uses = false;
+  int target_sdk_version = 29; /* Q */
   std::vector<std::string> app_class_name_filter;
+  std::vector<std::string> exclude_api_lists;
 };
 
 static const char* Substr(const char* str, int index) {
@@ -108,13 +108,14 @@ static void ParseArgs(VeridexOptions* options, int argc, char** argv) {
       options->precise = false;
     } else if (StartsWith(argv[i], kTargetSdkVersion)) {
       options->target_sdk_version = atoi(Substr(argv[i], strlen(kTargetSdkVersion)));
-    } else if (strcmp(argv[i], kOnlyReportSdkUses) == 0) {
-      options->only_report_sdk_uses = true;
     } else if (StartsWith(argv[i], kAppClassFilter)) {
       options->app_class_name_filter = android::base::Split(
           Substr(argv[i], strlen(kAppClassFilter)), ",");
+    } else if (StartsWith(argv[i], kExcludeApiListsOption)) {
+      options->exclude_api_lists = android::base::Split(
+          Substr(argv[i], strlen(kExcludeApiListsOption)), ",");
     } else {
-      LOG(WARNING) << "Unknown command line argument: " << argv[i];
+      LOG(ERROR) << "Unknown command line argument: " << argv[i];
     }
   }
 }
@@ -228,9 +229,10 @@ class Veridex {
     Resolve(app_dex_files, resolver_map, type_map, &app_resolvers);
 
     ClassFilter app_class_filter(options.app_class_name_filter);
+    ApiListFilter api_list_filter(options.exclude_api_lists);
 
     // Find and log uses of hidden APIs.
-    HiddenApi hidden_api(options.flags_file, options.only_report_sdk_uses);
+    HiddenApi hidden_api(options.flags_file, api_list_filter);
     HiddenApiStats stats;
 
     HiddenApiFinder api_finder(hidden_api);
@@ -243,7 +245,7 @@ class Veridex {
       precise_api_finder.Dump(std::cout, &stats);
     }
 
-    DumpSummaryStats(std::cout, stats, options);
+    DumpSummaryStats(std::cout, stats, api_list_filter);
 
     if (options.precise) {
       std::cout << "To run an analysis that can give more reflection accesses, " << std::endl
@@ -256,20 +258,15 @@ class Veridex {
  private:
   static void DumpSummaryStats(std::ostream& os,
                                const HiddenApiStats& stats,
-                               const VeridexOptions& options) {
+                               const ApiListFilter& api_list_filter) {
     static const char* kPrefix = "       ";
-    if (options.only_report_sdk_uses) {
-      os << stats.api_counts[hiddenapi::ApiList::Whitelist().GetIntValue()]
-         << " SDK API uses." << std::endl;
-    } else {
-      os << stats.count << " hidden API(s) used: "
-         << stats.linking_count << " linked against, "
-         << stats.reflection_count << " through reflection" << std::endl;
-      for (size_t i = 0; i < hiddenapi::ApiList::kValueCount; ++i) {
-        hiddenapi::ApiList api_list = hiddenapi::ApiList(i);
-        if (api_list != hiddenapi::ApiList::Whitelist()) {
-          os << kPrefix << stats.api_counts[i] << " in " << api_list << std::endl;
-        }
+    os << stats.count << " hidden API(s) used: "
+       << stats.linking_count << " linked against, "
+       << stats.reflection_count << " through reflection" << std::endl;
+    for (size_t i = 0; i < hiddenapi::ApiList::kValueCount; ++i) {
+      hiddenapi::ApiList api_list = hiddenapi::ApiList(i);
+      if (api_list_filter.Matches(api_list)) {
+        os << kPrefix << stats.api_counts[i] << " in " << api_list << std::endl;
       }
     }
   }