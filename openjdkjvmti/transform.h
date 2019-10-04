3732beb8c1b5fc722e4d993725aff1170239b181
Alex Light <allight@google.com>
2019-10-04 13:35:34 -0700

    Refactor Transform/redefine entrypoints
    The RetransformClasses and RedefineClasses entrypoints were previously
    partly defined in OpenjdkJvmTi.cc. Most other entrypoints are entirely
    defined in other files with only basic capability checks occurring in
    OpenjdkJvmTi.cc. This change brings these functions into alignment
    with all the others.
    
    Test: ./test.py --host
    Bug: 134162467
    Change-Id: I1e2e6506c89ad0e393dacdbc7f2d96659678397e

--- openjdkjvmti/transform.h
+++ openjdkjvmti/transform.h
@@ -48,7 +48,7 @@ jvmtiError GetClassLocation(ArtJvmTiEnv* env, jclass klass, /*out*/std::string* 
 
 class Transformer {
  public:
-  static void Setup();
+  static void Register(EventHandler* eh);
 
   template<ArtJvmtiEvent kEvent>
   static void TransformSingleClassDirect(
@@ -57,17 +57,12 @@ class Transformer {
       /*in-out*/ArtClassDefinition* def);
 
   static jvmtiError RetransformClassesDirect(
-      EventHandler* event_handler,
       art::Thread* self,
       /*in-out*/std::vector<ArtClassDefinition>* definitions);
 
-  static jvmtiError RetransformClasses(ArtJvmTiEnv* env,
-                                       EventHandler* event_handler,
-                                       art::Runtime* runtime,
-                                       art::Thread* self,
+  static jvmtiError RetransformClasses(jvmtiEnv* env,
                                        jint class_count,
-                                       const jclass* classes,
-                                       /*out*/std::string* error_msg);
+                                       const jclass* classes);
 };
 
 }  // namespace openjdkjvmti
