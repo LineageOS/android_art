50c812abbe8a8f64d2def49c549eaa005b5f32c7
Vladimir Marko <vmarko@google.com>
2019-10-15 08:57:00 +0000

    Revert "Refactor oat file writing."
    This reverts commit 0d192d2c08116cab8244a6eb229aa784bc6cfefa.
    
    Reason for revert: Boot time regression.
    Bug: 142680736
    
    Change-Id: I65c8f687668c1d7cec293b6b8bfe7147181c0a10

--- dex2oat/linker/oat_writer_test.cc
+++ dex2oat/linker/oat_writer_test.cc
@@ -178,6 +178,8 @@ class OatTest : public CommonCompilerDriverTest {
     std::vector<std::unique_ptr<const DexFile>> opened_dex_files;
     if (!oat_writer.WriteAndOpenDexFiles(
         vdex_file,
+        oat_rodata,
+        &key_value_store,
         verify,
         /*update_input_vdex=*/ false,
         copy,
@@ -197,13 +199,7 @@ class OatTest : public CommonCompilerDriverTest {
     MultiOatRelativePatcher patcher(compiler_options_->GetInstructionSet(),
                                     compiler_options_->GetInstructionSetFeatures(),
                                     compiler_driver_->GetCompiledMethodStorage());
-    if (!oat_writer.StartRoData(compiler_driver_.get(),
-                                /*image_writer=*/ nullptr,
-                                dex_files,
-                                oat_rodata,
-                                &key_value_store)) {
-      return false;
-    }
+    oat_writer.Initialize(compiler_driver_.get(), nullptr, dex_files);
     oat_writer.PrepareLayout(&patcher);
     elf_writer->PrepareDynamicSection(oat_writer.GetOatHeader().GetExecutableOffset(),
                                       oat_writer.GetCodeSize(),
