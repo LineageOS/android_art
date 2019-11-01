9f459c4ef60d771fc4eb8da1f709f479ca23a6ab
Colin Cross <ccross@android.com>
2019-11-01 18:23:06 +0000

    Revert "Boot image extension."
    This reverts commit 4f6ebe89af462ff05d97433deb2bca3f3b011fc2.
    
    Reason for revert: b/143492855
    
    Bug: 119800099
    Bug: 143492855
    Change-Id: I27da7e790f61f836701f8f39c98d14b507af8b9e

--- runtime/oat.h
+++ runtime/oat.h
@@ -32,8 +32,8 @@ class InstructionSetFeatures;
 class PACKED(4) OatHeader {
  public:
   static constexpr std::array<uint8_t, 4> kOatMagic { { 'o', 'a', 't', '\n' } };
-  // Last oat version changed reason: Boot image extension.
-  static constexpr std::array<uint8_t, 4> kOatVersion { { '1', '7', '4', '\0' } };
+  // Last oat version changed reason: Optimize stack map decoding - interleave varints.
+  static constexpr std::array<uint8_t, 4> kOatVersion { { '1', '7', '3', '\0' } };
 
   static constexpr const char* kDex2OatCmdLineKey = "dex2oat-cmdline";
   static constexpr const char* kDebuggableKey = "debuggable";
