2f8c1ac61b0c611d67badea70261c851ed19b82a
Alex Light <allight@google.com>
2019-11-19 01:09:31 +0000

    Revert^3 "Initial support for adding virtuals with structural redefinition"
    This reverts commit 8ba9738d55c11dfb7f85a7de9b1d6a4e1225ad20.
    
    Reason for revert: Seems to be failing with `Check failed: method->IsCompilable()`
    
    Test: None
    Bug: 134162467
    Bug: 144168550
    Bug: 144729319
    Change-Id: Ida9228fa171ae16d803b5ad4438e233a2ed4ffb2

--- openjdkjvmti/ti_redefine.h
+++ openjdkjvmti/ti_redefine.h
@@ -41,7 +41,6 @@
 #include "art_jvmti.h"
 #include "base/array_ref.h"
 #include "base/globals.h"
-#include "dex/class_accessor.h"
 #include "dex/dex_file.h"
 #include "dex/dex_file_structs.h"
 #include "jni/jni_env_ext-inl.h"
@@ -156,9 +155,6 @@ class Redefiner {
     bool FinishRemainingAllocations(/*out*/RedefinitionDataIter* cur_data)
         REQUIRES_SHARED(art::Locks::mutator_lock_);
 
-    bool CollectAndCreateNewInstances(/*out*/RedefinitionDataIter* cur_data)
-        REQUIRES_SHARED(art::Locks::mutator_lock_);
-
     bool AllocateAndRememberNewDexFileCookie(
         art::Handle<art::mirror::ClassLoader> source_class_loader,
         art::Handle<art::mirror::Object> dex_file_obj,
@@ -238,14 +234,8 @@ class Redefiner {
 
     void RecordNewMethodAdded();
     void RecordNewFieldAdded();
-    void RecordHasVirtualMembers() {
-      has_virtuals_ = true;
-    }
 
-    bool HasVirtualMembers() const {
-      return has_virtuals_;
-    }
-
+   private:
     bool IsStructuralRedefinition() const {
       DCHECK(!(added_fields_ || added_methods_) || driver_->IsStructuralRedefinition())
           << "added_fields_: " << added_fields_ << " added_methods_: " << added_methods_
@@ -253,7 +243,6 @@ class Redefiner {
       return driver_->IsStructuralRedefinition() && (added_fields_ || added_methods_);
     }
 
-   private:
     void UpdateClassStructurally(const RedefinitionDataIter& cur_data)
         REQUIRES(art::Locks::mutator_lock_);
 
@@ -268,7 +257,6 @@ class Redefiner {
 
     bool added_fields_ = false;
     bool added_methods_ = false;
-    bool has_virtuals_ = false;
 
     // Does the class need to be reverified due to verification soft-fails possibly forcing
     // interpreter or lock-counting?
@@ -322,8 +310,6 @@ class Redefiner {
   bool EnsureAllClassAllocationsFinished(RedefinitionDataHolder& holder)
       REQUIRES_SHARED(art::Locks::mutator_lock_);
   bool FinishAllRemainingAllocations(RedefinitionDataHolder& holder)
-      REQUIRES_SHARED(art::Locks::mutator_lock_);
-  bool CollectAndCreateNewInstances(RedefinitionDataHolder& holder)
       REQUIRES_SHARED(art::Locks::mutator_lock_);
   void ReleaseAllDexFiles() REQUIRES_SHARED(art::Locks::mutator_lock_);
   void ReverifyClasses(RedefinitionDataHolder& holder) REQUIRES_SHARED(art::Locks::mutator_lock_);
