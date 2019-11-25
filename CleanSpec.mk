621738039bbca55a2513bddb6ea66d4934fa7089
Jooyung Han <jooyung@google.com>
2019-11-25 13:46:35 +0900

    Bind-mount individual flattened APEXes
    There are a few directories for builtin APEXes: /system/apex,
    /system_ext/apex, /product/apex, /vendor/apex.
    
    For devices with ro.apex.updatable=false, init should bind-mount
    "flattened" apexes from all of above.
    
    Instead of making mount point for "com.android.art" at buildtime, init
    process creates it before mounting.
    
    Bug: 144732372
    Test: OVERRIDE_TARGET_FLATTEN_APEX=true m && device boot
    Change-Id: I64f0d6bdd1d297ccc0679fb6319103d5bfbfc620

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -102,6 +102,9 @@ $(call add-clean-step, find $(OUT_DIR) -name "*.oat" -o -name "*.odex" -o -name 
 $(call add-clean-step, find $(OUT_DIR) -name "*.oat" -o -name "*.odex" -o -name "*.art" -o -name '*.vdex' | xargs rm -f)
 $(call add-clean-step, find $(OUT_DIR) -name "*.oat" -o -name "*.odex" -o -name "*.art" -o -name '*.vdex' | xargs rm -f)
 
+# Remove empty dir for art APEX because it will be created on demand while mounting release|debug
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/apex/com.android.art)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
