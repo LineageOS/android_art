d6b07c53088f3b9a75a5df4b3128b7497c7a70e4
Andreas Gampe <agampe@fb.com>
2020-03-03 20:02:26 +0000

    ART: Enable testing on non-UTF-8 platform
    Some of the test files use UTF-encoded sources. Ensure that javac
    knows that instead of picking up the encoding of the host platform.
    
    Ensure that testrunner reads test output with the correct
    encoding.
    
    Test: art/test/testrunner/testrunner.py -b --host -j 20 -t 2005
    Change-Id: I4fe38fd32a12edcae56a7ba559df5828f0992428

--- tools/javac-helper.sh
+++ tools/javac-helper.sh
@@ -97,6 +97,7 @@ else
   bcp_arg="$(join_by ":" "${javac_bootclasspath[@]}")"
   javac_args=(-bootclasspath "$bcp_arg")
 fi
+javac_args+=(-encoding utf8)
 
 if [[ $showcommands == y ]]; then
   echo ${JAVAC} "${javac_args[@]}" "$@"
