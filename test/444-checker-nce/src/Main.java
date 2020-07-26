hopez13/art

More inclusive language fixes
Bug: 161896447
Bug: 161850439
Bug: 161336379
Test: art/test.py --host
Change-Id: I1519e22d40cb28f243dd75b12d455cfa844726fc

test/444-checker-nce/src/Main.java
@@ -227,7 +227,7 @@ public class Main {
   }
 
   public Main() {}
-  public Main(int dummy) {}
+  public Main(int placeholder) {}
 
   private Main g() {
     // avoids inlining

test/569-checker-pattern-replacement/src-multidex/Base.java
@@ -59,7 +59,7 @@ public class Base {
     this(0.0, objectValue);
   }
 
-  Base(int intValue, long dummy) {
+  Base(int intValue, long placeholder) {
     this(intValue, 0.0, null);
   }
 

test/595-profile-saving/src/Main.java
@@ -70,7 +70,7 @@ public class Main {
   // Returns true if the profile is for the boot image.
   public static native boolean isForBootImage(String profile);
 
-  private static final String TEMP_FILE_NAME_PREFIX = "dummy";
+  private static final String TEMP_FILE_NAME_PREFIX = "placeholder";
   private static final String TEMP_FILE_NAME_SUFFIX = "-file";
 
   static native String getProfileInfoDump(

test/956-methodhandles/src/Main.java
@@ -1172,13 +1172,13 @@ public class Main {
 
     // boolean -> int
     try {
-        int dummy = (int) mh.invoke("True");
+        int placeholder = (int) mh.invoke("True");
         fail();
     } catch (WrongMethodTypeException e) {}
 
     // boolean -> Integer
     try {
-        Integer dummy = (Integer) mh.invoke("True");
+        Integer placeholder = (Integer) mh.invoke("True");
         fail();
     } catch (WrongMethodTypeException e) {}
 
@@ -1190,13 +1190,13 @@ public class Main {
 
     // Boolean -> int
     try {
-        int dummy = (int) mh.invoke(false);
+        int placeholder = (int) mh.invoke(false);
         fail();
     } catch (WrongMethodTypeException e) {}
 
     // Boolean -> Integer
     try {
-        Integer dummy = (Integer) mh.invoke("True");
+        Integer placeholder = (Integer) mh.invoke("True");
         fail();
     } catch (WrongMethodTypeException e) {}
 

LineageOS/android_art

test/444-checker-nce/src/Main.java
@@ -227,7 +227,7 @@ public class Main {
   }
 
   public Main() {}
-  public Main(int dummy) {}
+  public Main(int unused) {}
 
   private Main g() {
     // avoids inlining

test/569-checker-pattern-replacement/src-multidex/Base.java
@@ -59,7 +59,7 @@ public class Base {
     this(0.0, objectValue);
   }
 
-  Base(int intValue, long dummy) {
+  Base(int intValue, long unused) {
     this(intValue, 0.0, null);
   }
 

test/595-profile-saving/src/Main.java
@@ -70,7 +70,7 @@ public class Main {
   // Returns true if the profile is for the boot image.
   public static native boolean isForBootImage(String profile);
 
-  private static final String TEMP_FILE_NAME_PREFIX = "dummy";
+  private static final String TEMP_FILE_NAME_PREFIX = "temp";
   private static final String TEMP_FILE_NAME_SUFFIX = "-file";
 
   static native String getProfileInfoDump(

test/956-methodhandles/src/Main.java
@@ -1172,13 +1172,13 @@ public class Main {
 
     // boolean -> int
     try {
-        int dummy = (int) mh.invoke("True");
+        int unexpectedValue = (int) mh.invoke("True");
         fail();
     } catch (WrongMethodTypeException e) {}
 
     // boolean -> Integer
     try {
-        Integer dummy = (Integer) mh.invoke("True");
+        Integer unexpectedValue = (Integer) mh.invoke("True");
         fail();
     } catch (WrongMethodTypeException e) {}
 
@@ -1190,13 +1190,13 @@ public class Main {
 
     // Boolean -> int
     try {
-        int dummy = (int) mh.invoke(false);
+        int unexpectedValue = (int) mh.invoke(false);
         fail();
     } catch (WrongMethodTypeException e) {}
 
     // Boolean -> Integer
     try {
-        Integer dummy = (Integer) mh.invoke("True");
+        Integer unexpectedValue = (Integer) mh.invoke("True");
         fail();
     } catch (WrongMethodTypeException e) {}
 