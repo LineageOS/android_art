72e27b0b29fd1bdea6747f62c5bd865d26a0bdcd
Alex Light <allight@google.com>
2020-07-27 12:59:58 -0700

    Remove deprecated debug_suspend_count TLS value
    The debug_suspend_count TLS value has been dead for a while and was
    accidentally left in. Remove it entirely.
    
    Test: ./test.py --host
    Change-Id: Ie2ead0d30e5ff3885cdd83242cad2c826c7fb732

--- runtime/thread.h
+++ runtime/thread.h
@@ -255,10 +255,6 @@ class Thread {
     return tls32_.user_code_suspend_count;
   }
 
-  int GetDebugSuspendCount() const REQUIRES(Locks::thread_suspend_count_lock_) {
-    return tls32_.debug_suspend_count;
-  }
-
   bool IsSuspended() const {
     union StateAndFlags state_and_flags;
     state_and_flags.as_int = tls32_.state_and_flags.as_int;
@@ -1541,7 +1537,6 @@ class Thread {
 
     explicit tls_32bit_sized_values(bool is_daemon)
         : suspend_count(0),
-          debug_suspend_count(0),
           thin_lock_thread_id(0),
           tid(0),
           daemon(is_daemon),
@@ -1568,10 +1563,6 @@ class Thread {
     // A non-zero value is used to tell the current thread to enter a safe point
     // at the next poll.
     int suspend_count GUARDED_BY(Locks::thread_suspend_count_lock_);
-
-    // How much of 'suspend_count_' is by request of the debugger, used to set things right
-    // when the debugger detaches. Must be <= suspend_count_.
-    int debug_suspend_count GUARDED_BY(Locks::thread_suspend_count_lock_);
 
     // Thin lock thread id. This is a small integer used by the thin lock implementation.
     // This is not to be confused with the native thread's tid, nor is it the value returned
