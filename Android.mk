hopez13/art

Upper-case the i18n host data path for consistency.
Bug: 157734636
Test: m
Change-Id: I6a79114b63c020fe77f1cc51faab8664802970c6

Android.mk
@@ -469,7 +469,7 @@ endif
 build-art: build-art-host build-art-target
 
 # For host, we extract the ICU data from the apex and install it to HOST_OUT/I18N_APEX.
-$(host-i18n-data-timestamp): $(TARGET_OUT)/apex/$(I18N_APEX).apex $(HOST_OUT)/bin/deapexer
+$(HOST_i18N_DATA_FILE): $(TARGET_OUT)/apex/$(I18N_APEX).apex $(HOST_OUT)/bin/deapexer
 	$(call extract-from-apex,$(I18N_APEX))
 	rm -rf $(HOST_OUT)/$(I18N_APEX)
 	mkdir -p $(HOST_OUT)/$(I18N_APEX)/
@@ -477,7 +477,7 @@ $(host-i18n-data-timestamp): $(TARGET_OUT)/apex/$(I18N_APEX).apex $(HOST_OUT)/bi
 	touch $@
 
 .PHONY: build-art-host
-build-art-host:   $(HOST_OUT_EXECUTABLES)/art $(ART_HOST_DEPENDENCIES) $(HOST_CORE_IMG_OUTS) $(host-i18n-data-timestamp)
+build-art-host:   $(HOST_OUT_EXECUTABLES)/art $(ART_HOST_DEPENDENCIES) $(HOST_CORE_IMG_OUTS) $(HOST_I18N_DATA_FILE)
 
 .PHONY: build-art-target
 build-art-target: $(TARGET_OUT_EXECUTABLES)/art $(ART_TARGET_DEPENDENCIES) $(TARGET_CORE_IMG_OUTS)

build/Android.common_path.mk
@@ -128,6 +128,6 @@ CONSCRYPT_APEX := com.android.conscrypt
 I18N_APEX := com.android.i18n
 
 # A phony file to create the ICU data file for host.
-host-i18n-data-timestamp := $(HOST_OUT)/$(I18N_APEX)/timestamp
+HOST_I18N_DATA_FILE := $(HOST_OUT)/$(I18N_APEX)/timestamp
 
 endif # ART_ANDROID_COMMON_PATH_MK

build/Android.gtest.mk
@@ -256,7 +256,7 @@ endif
   gtest_suffix :=
 endef  # define-art-gtest-rule-host
 
-ART_TEST_HOST_GTEST_DEPENDENCIES := $(host-i18n-data-timestamp)
+ART_TEST_HOST_GTEST_DEPENDENCIES := $(HOST_I18N_DATA_FILE)
 ART_TEST_TARGET_GTEST_DEPENDENCIES := $(TESTING_ART_APEX)
 
 # Add the additional dependencies for the specified test

LineageOS/android_art

Android.mk
@@ -469,7 +469,7 @@ endif
 build-art: build-art-host build-art-target
 
 # For host, we extract the ICU data from the apex and install it to HOST_OUT/I18N_APEX.
-$(host-i18n-data-timestamp): $(TARGET_OUT)/apex/$(I18N_APEX).apex $(HOST_OUT)/bin/deapexer
+$(HOST_I18N_DATA): $(TARGET_OUT)/apex/$(I18N_APEX).apex $(HOST_OUT)/bin/deapexer
 	$(call extract-from-apex,$(I18N_APEX))
 	rm -rf $(HOST_OUT)/$(I18N_APEX)
 	mkdir -p $(HOST_OUT)/$(I18N_APEX)/
@@ -477,7 +477,7 @@ $(host-i18n-data-timestamp): $(TARGET_OUT)/apex/$(I18N_APEX).apex $(HOST_OUT)/bi
 	touch $@
 
 .PHONY: build-art-host
-build-art-host:   $(HOST_OUT_EXECUTABLES)/art $(ART_HOST_DEPENDENCIES) $(HOST_CORE_IMG_OUTS) $(host-i18n-data-timestamp)
+build-art-host:   $(HOST_OUT_EXECUTABLES)/art $(ART_HOST_DEPENDENCIES) $(HOST_CORE_IMG_OUTS) $(HOST_I18N_DATA)
 
 .PHONY: build-art-target
 build-art-target: $(TARGET_OUT_EXECUTABLES)/art $(ART_TARGET_DEPENDENCIES) $(TARGET_CORE_IMG_OUTS)

build/Android.common_path.mk
@@ -128,6 +128,6 @@ CONSCRYPT_APEX := com.android.conscrypt
 I18N_APEX := com.android.i18n
 
 # A phony file to create the ICU data file for host.
-host-i18n-data-timestamp := $(HOST_OUT)/$(I18N_APEX)/timestamp
+HOST_I18N_DATA := $(HOST_OUT)/$(I18N_APEX)/timestamp
 
 endif # ART_ANDROID_COMMON_PATH_MK

build/Android.gtest.mk
@@ -256,7 +256,7 @@ endif
   gtest_suffix :=
 endef  # define-art-gtest-rule-host
 
-ART_TEST_HOST_GTEST_DEPENDENCIES := $(host-i18n-data-timestamp)
+ART_TEST_HOST_GTEST_DEPENDENCIES := $(HOST_I18N_DATA)
 ART_TEST_TARGET_GTEST_DEPENDENCIES := $(TESTING_ART_APEX)
 
 # Add the additional dependencies for the specified test