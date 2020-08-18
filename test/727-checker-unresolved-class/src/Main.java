cfd65805a5b3b0437e355b8044a05ee6c9d352c5
Vladimir Marko <vmarko@google.com>
2020-08-18 09:29:51 +0100

    Optimizing: Fix weak method access check.
    And improve generated code for accessing package private
    methods from unresolved compiling class in the same package.
    
    Test: Additional test in 727-checker-unresolved-class
    Test: testrunner.py --host --optimizing --interpreter --jvm -t 727
    Test: testrunner.py --host --optimizing
    Bug: 161898207
    Change-Id: Ia34552d90620e8e0398099522a5a52b4a45df15d

--- test/727-checker-unresolved-class/src/Main.java
+++ test/727-checker-unresolved-class/src/Main.java
@@ -14,11 +14,31 @@
  * limitations under the License.
  */
 
-import unresolved.UnresolvedClass;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class Main {
-  public static void main(String[] args) {
-    UnresolvedClass.$noinline$main();
-    System.out.println("passed");
+  public static String TEST_NAME = "727-checker-unresolved-class";
+
+  public static ClassLoader getClassLoaderFor(String location) throws Exception {
+    try {
+      Class<?> class_loader_class = Class.forName("dalvik.system.PathClassLoader");
+      Constructor<?> ctor =
+          class_loader_class.getConstructor(String.class, ClassLoader.class);
+      /* on Dalvik, this is a DexFile; otherwise, it's null */
+      return (ClassLoader) ctor.newInstance(location + "/" + TEST_NAME + "-ex.jar",
+                                            Main.class.getClassLoader());
+    } catch (ClassNotFoundException e) {
+      // Running on RI. Use URLClassLoader.
+      return new java.net.URLClassLoader(
+          new java.net.URL[] { new java.net.URL("file://" + location + "/classes-ex/") });
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    ClassLoader new_loader = getClassLoaderFor(System.getenv("DEX_LOCATION"));
+    Class<?> testClass = Class.forName("Test", true, new_loader);
+    Method testMain = testClass.getMethod("$noinline$main");
+    testMain.invoke(null);
   }
 }
