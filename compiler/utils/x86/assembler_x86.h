hopez13/art

X86: Add the other set VarHandles (setVolatile, setRelease, setOpaque)
This commit implements VarHandle.setVolatile, setRelease and setOpaque
intrisics. This also implied refactoring HandleFieldSet to be reused in
all set VarHandles, as the code is very similar.

Test: ART_HEAP_POISONING=true art/test.py --host --all-compiler -r --32
Test: ART_HEAP_POISONING=false art/test.py --host --all-compiler -r --32
Test: ART_USE_READ_BARRIER=true art/test.py --host --all-compiler -r --32
Test: ART_USE_READ_BARRIER=false art/test.py --host --all-compiler -r --32
Bug: 65872996
Change-Id: I9a1d5fec6c5086c1e77ba65c3337da1133b3e3f1

compiler/utils/x86/assembler_x86.h
@@ -73,6 +73,10 @@ class Operand : public ValueObject {
     return static_cast<Register>(encoding_at(1) & 7);
   }
 
+  int32_t disp() const {
+    return disp_;
+  }
+
   int8_t disp8() const {
     CHECK_GE(length_, 2);
     return static_cast<int8_t>(encoding_[length_ - 1]);
@@ -92,7 +96,7 @@ class Operand : public ValueObject {
 
  protected:
   // Operand can be sub classed (e.g: Address).
-  Operand() : length_(0), fixup_(nullptr) { }
+  Operand() : length_(0), disp_(0), fixup_(nullptr) { }
 
   void SetModRM(int mod_in, Register rm_in) {
     CHECK_EQ(mod_in & ~3, 0);
@@ -110,6 +114,7 @@ class Operand : public ValueObject {
   void SetDisp8(int8_t disp) {
     CHECK(length_ == 1 || length_ == 2);
     encoding_[length_++] = static_cast<uint8_t>(disp);
+    disp_ = disp;
   }
 
   void SetDisp32(int32_t disp) {
@@ -117,6 +122,7 @@ class Operand : public ValueObject {
     int disp_size = sizeof(disp);
     memmove(&encoding_[length_], &disp, disp_size);
     length_ += disp_size;
+    disp_ = disp;
   }
 
   AssemblerFixup* GetFixup() const {
@@ -130,12 +136,13 @@ class Operand : public ValueObject {
  private:
   uint8_t length_;
   uint8_t encoding_[6];
+  int32_t disp_;
 
   // A fixup can be associated with the operand, in order to be applied after the
   // code has been generated. This is used for constant area fixups.
   AssemblerFixup* fixup_;
 
-  explicit Operand(Register reg) : fixup_(nullptr) { SetModRM(3, reg); }
+  explicit Operand(Register reg) : disp_(0), fixup_(nullptr) { SetModRM(3, reg); }
 
   // Get the operand encoding byte at the given index.
   uint8_t encoding_at(int index_in) const {
@@ -189,6 +196,15 @@ class Address : public Operand {
           int32_t disp, AssemblerFixup *fixup) {
     Init(base_in, index_in, scale_in, disp);
     SetFixup(fixup);
+  }
+
+  Address displaceBy(int bits_count) {
+    if (rm() == ESP) {
+      // SIB addressing mode
+      return Address(base(), index(), scale(), disp() + bits_count, GetFixup());
+    } else {
+      return Address(rm(), disp() + bits_count, GetFixup());
+    }
   }
 
   static Address Absolute(uintptr_t addr) {

LineageOS/android_art

compiler/utils/x86/assembler_x86.h
@@ -73,6 +73,10 @@ class Operand : public ValueObject {
     return static_cast<Register>(encoding_at(1) & 7);
   }
 
+  int32_t disp() const {
+    return disp_;
+  }
+
   int8_t disp8() const {
     CHECK_GE(length_, 2);
     return static_cast<int8_t>(encoding_[length_ - 1]);
@@ -92,7 +96,7 @@ class Operand : public ValueObject {
 
  protected:
   // Operand can be sub classed (e.g: Address).
-  Operand() : length_(0), fixup_(nullptr) { }
+  Operand() : length_(0), disp_(0), fixup_(nullptr) { }
 
   void SetModRM(int mod_in, Register rm_in) {
     CHECK_EQ(mod_in & ~3, 0);
@@ -110,6 +114,7 @@ class Operand : public ValueObject {
   void SetDisp8(int8_t disp) {
     CHECK(length_ == 1 || length_ == 2);
     encoding_[length_++] = static_cast<uint8_t>(disp);
+    disp_ = disp;
   }
 
   void SetDisp32(int32_t disp) {
@@ -117,6 +122,7 @@ class Operand : public ValueObject {
     int disp_size = sizeof(disp);
     memmove(&encoding_[length_], &disp, disp_size);
     length_ += disp_size;
+    disp_ = disp;
   }
 
   AssemblerFixup* GetFixup() const {
@@ -130,12 +136,13 @@ class Operand : public ValueObject {
  private:
   uint8_t length_;
   uint8_t encoding_[6];
+  int32_t disp_;
 
   // A fixup can be associated with the operand, in order to be applied after the
   // code has been generated. This is used for constant area fixups.
   AssemblerFixup* fixup_;
 
-  explicit Operand(Register reg) : fixup_(nullptr) { SetModRM(3, reg); }
+  explicit Operand(Register reg) : disp_(0), fixup_(nullptr) { SetModRM(3, reg); }
 
   // Get the operand encoding byte at the given index.
   uint8_t encoding_at(int index_in) const {
@@ -189,6 +196,15 @@ class Address : public Operand {
           int32_t disp, AssemblerFixup *fixup) {
     Init(base_in, index_in, scale_in, disp);
     SetFixup(fixup);
+  }
+
+  Address displaceBy(int offset) {
+    if (rm() == ESP) {
+      // SIB addressing mode
+      return Address(base(), index(), scale(), disp() + offset, GetFixup());
+    } else {
+      return Address(rm(), disp() + offset, GetFixup());
+    }
   }
 
   static Address Absolute(uintptr_t addr) {