e4f2215df60f0408d37ec796ececaa32b860d37a
Eric Holk <eholk@google.com>
2020-11-17 15:23:33 -0800

    [metrics] Move metrics code to runtime
    The background reporting thread is going to need tighter ties to the
    Runtime object, so it makes sense to bring the rest of the metrics code
    under runtime. Since these are mostly runtime metrics, this is a more
    logical place for them anyway.
    
    Test: m test-art-host-gtest-art_runtime_tests
    Bug: 170149255
    Change-Id: Ib425041c41a5283237af8dd9978582e605a89a31

--- runtime/verifier/method_verifier_test.cc
+++ runtime/verifier/method_verifier_test.cc
@@ -21,12 +21,12 @@
 #include <memory>
 
 #include "android-base/strings.h"
-#include "base/metrics_test.h"
 #include "base/utils.h"
 #include "class_linker-inl.h"
 #include "class_verifier.h"
 #include "common_runtime_test.h"
 #include "dex/dex_file-inl.h"
+#include "metrics/metrics_test.h"
 #include "scoped_thread_state_change-inl.h"
 #include "verifier_enums.h"
 
