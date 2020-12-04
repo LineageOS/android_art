9e428ad581eb75e0bee5322ac95bcab36064cbf9
Vladimir Marko <vmarko@google.com>
2020-12-04 15:50:30 +0000

    Add SafetyNet logging to JNI::NewStringUTF.
    (cherry picked from commit ed4b3e0958d3de6a92d82abb9f81e49e84d5c673)
    
    Test: blueline-userdebug boots.
    Bug: 172655291
    Merged-In: I653db8be0c0a45302f0d1c54285c02d2d052a9f4
    Change-Id: I653db8be0c0a45302f0d1c54285c02d2d052a9f4
    (cherry picked from commit 69fc841b8460943c2b2224f61585942cbc9f3f40)

--- runtime/jni/jni_internal.cc
+++ runtime/jni/jni_internal.cc
@@ -17,6 +17,7 @@
 #include "jni_internal.h"
 
 #include <cstdarg>
+#include <log/log.h>
 #include <memory>
 #include <mutex>
 #include <utility>
@@ -2075,6 +2076,7 @@ class JNI {
         /*bad=*/ []() { return true; });  // Abort processing and return 0 for bad characters.
     if (UNLIKELY(utf8_length != 0u && utf16_length == 0u)) {
       // VisitModifiedUtf8Chars() aborted for a bad character.
+      android_errorWriteLog(0x534e4554, "172655291");  // Report to SafetyNet.
       // Report the error to logcat but avoid too much spam.
       static const uint64_t kMinDelay = UINT64_C(10000000000);  // 10s
       static std::atomic<uint64_t> prev_bad_input_time(UINT64_C(0));
