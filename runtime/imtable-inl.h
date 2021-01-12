hopez13/art

Improve invokeinterface for nterp.
- Remove bitwise negation of imt_index: the class linker already always
  initializes the imt_index.
- Special case the imt index of default methods: to simplify invocation
  in nterp, mask the dex_method_index to create the imt index.
- Add arm64, arm, x64 support in nterp.

Test: test.py
Bug: 112676029
Change-Id: I815a4a4ec5c219921ab4ed1a20b02586aab19a46

runtime/imtable-inl.h
@@ -83,6 +83,12 @@ inline void ImTable::GetImtHashComponents(ArtMethod* method,
 }
 
 inline uint32_t ImTable::GetImtIndex(ArtMethod* method) {
+  if (!method->IsAbstract()) {
+    // For default methods, where we cannot store the imt_index, we use the
+    // dex_method_index instead. We mask it with the closest power of two to
+    // simplify the interpreter.
+    return method->GetDexMethodIndex() & (ImTable::kSizeTruncToPowerOfTwo - 1);
+  }
   uint32_t class_hash, name_hash, signature_hash;
   GetImtHashComponents(method, &class_hash, &name_hash, &signature_hash);
 

LineageOS/android_art

  in nterp, mask the method_index to create the imt index.

runtime/imtable-inl.h
@@ -83,6 +83,12 @@ inline void ImTable::GetImtHashComponents(ArtMethod* method,
 }
 
 inline uint32_t ImTable::GetImtIndex(ArtMethod* method) {
+  if (!method->IsAbstract()) {
+    // For default methods, where we cannot store the imt_index, we use the
+    // method_index instead. We mask it with the closest power of two to
+    // simplify the interpreter.
+    return method->GetMethodIndex() & (ImTable::kSizeTruncToPowerOfTwo - 1);
+  }
   uint32_t class_hash, name_hash, signature_hash;
   GetImtHashComponents(method, &class_hash, &name_hash, &signature_hash);
 