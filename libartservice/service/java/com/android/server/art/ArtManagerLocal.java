b46231eb402f64c9a2d2983b11670eec9c8231ed
Paul Duffin <paulduffin@google.com>
2021-02-10 11:37:43 +0000

    Update Art Service to be a LocalManager
    This CL re-names the ArtService class to conform to the Frameworks
    naming convention for local managers, which are SystemServer only
    "service" singletons which are accessible through the LocalManagers
    registry.  The build system is updated for these changes, as well as
    adding appropriate API tracking build targets.
    
    Test: build, boot, and inspect log
    Bug: 177273468
    Merged-In: Iec3d2530a3a3a2824009b50f13e297da8fde55c5
    Change-Id: Iec3d2530a3a3a2824009b50f13e297da8fde55c5

--- libartservice/service/java/com/android/server/art/ArtManagerLocal.java
+++ libartservice/service/java/com/android/server/art/ArtManagerLocal.java
@@ -11,29 +11,18 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License
+ * limitations under the License.
  */
 
 package com.android.server.art;
 
-import static org.junit.Assert.assertEquals;
+/**
+ * This class provides a system API for functionality provided by the ART
+ * module.
+ */
+@libcore.api.CorePlatformApi(status = libcore.api.CorePlatformApi.Status.STABLE)
+public final class ArtManagerLocal {
+    static final String LOG_TAG = "ArtService";
 
-import com.android.server.art.ArtService;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-
-public class ArtServiceTests {
-    private static ArtService mArtService;
-
-    @Before
-    public void setup() {
-        mArtService = new ArtService();
-    }
-
-    @Test
-    public void testNotifyPrimaryUse() {
-        assertEquals(mArtService.getMsg(), "Hello world!");
-    }
+    public ArtManagerLocal() {}
 }
