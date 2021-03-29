26a759688bdd376db7ed429ae07e136ff21f2411
Martijn Coenen <maco@google.com>
2021-03-29 09:30:48 +0200

    Add on-device signing end-to-end test.
    This adds a host-driven test that verifies that installing an ART apex
    with a newer version generates artifacts and ensures they are signed
    with fs-verity. To that end, add a new ART test APEX which is identical
    to the regular ART APEX, except it carries a different version number.
    
    Bug: 165630556
    Bug: 167516462
    Test: atest odsign_e2e_tests
    Change-Id: If12f3922bbaafd875cb3120d01540e0b73d012be

---
+++ test/odsign/src/com/android/tests/odsign/ArtifactsSignedTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tests.odsign;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArtifactsSignedTest {
+    private static final String TAG = "VerifyArtArtifactsSignedTest";
+    private static final String ARTIFACTS_DIR = "/data/misc/apexdata/com.android.art/dalvik-cache";
+    private static final String FS_VERITY_PROC_PATH = "/proc/sys/fs/verity";
+
+    static {
+        System.loadLibrary("OdsignTestAppJni");
+    }
+
+    private static native boolean hasFsverityNative(@NonNull String path);
+
+    public boolean isFsVeritySupported() {
+        return new File(FS_VERITY_PROC_PATH).exists();
+    }
+
+    @Test
+    public void testArtArtifactsHaveFsverity() throws Exception {
+        assumeTrue("fs-verity is not supported on this device.", isFsVeritySupported());
+        List<File> files = Files.walk(Paths.get(ARTIFACTS_DIR), Integer.MAX_VALUE).
+            map(Path::toFile)
+            .collect(Collectors.toList());
+
+        for (File file : files) {
+            if (file.isFile()) {
+                assertTrue(file.getPath() + " is not in fs-verity",
+                        hasFsverityNative(file.getPath()));
+                Log.i(TAG, file.getPath() + " is in fs-verity");
+            }
+        }
+    }
+}
---
+++ test/odsign/test-src/com/android/tests/odsign/OnDeviceSigningHostTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tests.odsign;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+
+import static org.junit.Assume.assumeTrue;
+
+import android.cts.install.lib.host.InstallUtilsHost;
+
+import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
+import com.android.tradefed.device.ITestDevice.ApexInfo;
+import com.android.tradefed.testtype.junit4.BaseHostJUnit4Test;
+import com.android.tradefed.testtype.junit4.DeviceTestRunOptions;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.time.Duration;
+
+@RunWith(DeviceJUnit4ClassRunner.class)
+public class OnDeviceSigningHostTest extends BaseHostJUnit4Test {
+
+    private static final String APEX_FILENAME = "test_com.android.art.apex";
+
+    private static final String TEST_APP_PACKAGE_NAME = "com.android.tests.odsign";
+    private static final String TEST_APP_APK = "odsign_e2e_test_app.apk";
+
+    private final InstallUtilsHost mInstallUtils = new InstallUtilsHost(this);
+
+    private static final Duration BOOT_COMPLETE_TIMEOUT = Duration.ofMinutes(2);
+
+    @Before
+    public void setUp() throws Exception {
+        assumeTrue("Updating APEX is not supported", mInstallUtils.isApexUpdateSupported());
+    }
+
+    @Before // Generally not needed, but local test devices are sometimes in a "bad" start state.
+    @After
+    public void cleanup() throws Exception {
+        ApexInfo apex = mInstallUtils.getApexInfo(mInstallUtils.getTestFile(APEX_FILENAME));
+        getDevice().uninstallPackage(apex.name);
+        reboot();
+    }
+
+    @Test
+    public void verifyArtUpgradeSignsFiles() throws Exception {
+        installPackage(TEST_APP_APK);
+        mInstallUtils.installApexes(APEX_FILENAME);
+        reboot();
+        DeviceTestRunOptions options = new DeviceTestRunOptions(TEST_APP_PACKAGE_NAME);
+        options.setTestClassName(TEST_APP_PACKAGE_NAME + ".ArtifactsSignedTest");
+        options.setTestMethodName("testArtArtifactsHaveFsverity");
+        runDeviceTests(options);
+    }
+
+    private void reboot() throws Exception {
+        getDevice().reboot();
+        boolean success = getDevice().waitForBootComplete(BOOT_COMPLETE_TIMEOUT.toMillis());
+        assertWithMessage("Device didn't boot in %s", BOOT_COMPLETE_TIMEOUT).that(success).isTrue();
+    }
+}
