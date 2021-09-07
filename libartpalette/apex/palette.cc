hopez13/art

Add a lock contention palette hook.
Bug: 196334695
Test: m
Change-Id: Id87cfc30957baa5e575947fc69d6f307e843dcf8

libartpalette/apex/palette.cc
@@ -218,4 +218,22 @@ palette_status_t PaletteNotifyEndJniInvocation(JNIEnv* env) {
   return m(env);
 }
 
+palette_status_t PaletteReportLockContention(int32_t wait_ms,
+                                             const char* filename,
+                                             int32_t line_number,
+                                             const char* owner_filename,
+                                             int32_t owner_line_number,
+                                             const char* proc_name,
+                                             const char* thread_name) {
+  PaletteReportLockContentionMethod m =
+      PaletteLoader::Instance().GetPaletteReportLockContentionMethod();
+  return m(wait_ms,
+           filename,
+           line_number,
+           owner_filename,
+           owner_line_number,
+           proc_name,
+           thread_name);
+}
+
 }  // extern "C"

LineageOS/android_art

Merged-In: Id87cfc30957baa5e575947fc69d6f307e843dcf8
(cherry picked from commit e261356db98dc27bcec4048b3eaf9834864169aa)
(cherry picked from commit 977054f46299a940e4db186a99f91b14dd735576)

libartpalette/apex/palette.cc
@@ -218,4 +218,28 @@ palette_status_t PaletteNotifyEndJniInvocation(JNIEnv* env) {
   return m(env);
 }
 
+palette_status_t PaletteReportLockContention(JNIEnv* env,
+                                             int32_t wait_ms,
+                                             const char* filename,
+                                             int32_t line_number,
+                                             const char* method_name,
+                                             const char* owner_filename,
+                                             int32_t owner_line_number,
+                                             const char* owner_method_name,
+                                             const char* proc_name,
+                                             const char* thread_name) {
+  PaletteReportLockContentionMethod m =
+      PaletteLoader::Instance().GetPaletteReportLockContentionMethod();
+  return m(env,
+           wait_ms,
+           filename,
+           line_number,
+           method_name,
+           owner_filename,
+           owner_line_number,
+           owner_method_name,
+           proc_name,
+           thread_name);
+}
+
 }  // extern "C"